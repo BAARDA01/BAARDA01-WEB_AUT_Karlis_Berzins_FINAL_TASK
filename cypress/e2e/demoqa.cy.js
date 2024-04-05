ac4cd8a97678fa45828fe648181dfea7a34c727f
Kārlis Bērziņš <151729535+BAARDA01@users.noreply.github.com>
2024-04-05 23:45:51 +0300

    10%

--- cypress/e2e/demoqa.cy.js
+++ cypress/e2e/demoqa.cy.js
@@ -1,12 +1,50 @@
 import { SelectablePage } from "../pageObjects/selectable.page";
 
-describe("DemoQA", () => {
+describe("Automation Practice Form", () => {
   context("Grid", () => {
     beforeEach(() => {
       SelectablePage.visit();
     });
 
-    it("Grid", () => {
+    it("should fill out the form and submit", () => {
+
+       // Fill in personal information
+    type('#firstName', firstName);
+    type('#lastName', lastName);
+    type('#userEmail', email);
+    type('#userNumber', mobileNumber);
+    type('#currentAddress', address);
+
+    // Set Date of Birth
+    select('#dateOfBirthInput', '28 Feb 1930');
+
+    // Set Subjects
+    type('#subjectsInput', subjects);
+    click('.subjects-auto-complete__menu-list > div');
+
+    // Set Hobbies
+    click('#hobbiesWrapper input[value="Music"]');
+
+    // Upload an image
+    uploadFile('#uploadPicture', imagePath);
+
+    // Set State and City
+    select('#state', state);
+    select('#city', city);
+
+    // Submit the form
+    click('#submit');
+
+    // Validate labeled rows
+    cy.get('.table-responsive').should('contain', firstName);
+    cy.get('.table-responsive').should('contain', lastName);
+    cy.get('.table-responsive').should('contain', email);
+    cy.get('.table-responsive').should('contain', mobileNumber);
+    cy.get('.table-responsive').should('contain', address);
+    cy.get('.table-responsive').should('contain', subjects);
+    cy.get('.table-responsive').should('contain', hobbies);
+    cy.get('.table-responsive').should('contain', state);
+    cy.get('.table-responsive').should('contain', city);
 
      
    
